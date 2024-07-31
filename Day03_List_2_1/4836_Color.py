82e882cb8ab02216f59d947c075a710711ae4e68
PoloCeleste <voidness12@gmail.com>
2024-07-31 17:01:28 +0900

    update 07.31 17:01

---
+++ Day03_List_2_1/4836_Color.py
@@ -0,0 +1,20 @@
+﻿import sys
+sys.stdin = open('sample_input.txt', 'r') # 파일에서 입력받기
+
+for t in range(1, int(input())+1): # 테스트케이스 루프
+    N=int(input()) # 칠할 영역 개수
+    re = 0 # 결과값
+    arr = [[0]*10 for _ in range(10)] # 0으로 채워진 2차원 10X10 배열 생성
+    for n in range(N): # 영역 개수만큼 순회
+        info = list(map(int, sys.stdin.readline().split())) # 정보리스트 받기
+        for x in range(info[0], info[2]+1): # 칠할 영역 행 순회
+            for y in range(info[1], info[3]+1): # 칠할 영역 열 순회
+                arr[x][y]+=1 # 칠할 영역의 요소에 +1
+    for i in range(10): # 다 칠해진 영역 행 순회
+        for j in range(10): # 다 칠해진 영역 열 순회
+            if arr[i][j] == 2: # 만약 2번 칠해져서 보라색이 되었다면
+                ### 주어진 정보에서 같은 색인 영역은 겹치지 않는다. ###
+                ### 중복 고려 안해도 됨. 저 조건이 없고 중복이 발생하면 그거도 고려필 ###
+                re += 1 # 결과값 +1
+    
+    print(f'#{t} {re}') # 최종 출력
--- Day03_List_2_1/4837_Sum_Of_Subsets.py
+++ Day03_List_2_1/4837_Sum_Of_Subsets.py
@@ -1,9 +1,15 @@
-﻿arr = [1,2,3,4,5,6,7,8,9,10,11,12]
+﻿arr = [1,2,3,4,5,6,7,8,9,10,11,12] # 주어진 집합 A
 n = len(arr) # 원소의 개수
 
-for i in range(1<<n): # 1<<n : 부분 집합의 개수 (2진수)
-    for j in range(n): # 원소의 수만큼 비트를 비교함
-        if i & (1<<j): # i의 j번 비트가 1인 경우
-            print(arr[j], end=', ') # j번 원소 출력
-    print()
-print()
+for t in range(1, int(input())+1): # 테스트 케이스만큼 루프
+    re=0 # 해당하는 부분집합 개수
+    N, K = map(int, input().split()) # N개의 원소를 가지며 합이 K인 부분집합 찾기
+    for i in range(1<<n): # 1<<n : 부분 집합의 개수 (2진수)
+        a = [] # 빈 부분집합 생성
+        for j in range(n): # 원소의 수만큼 비트를 비교함
+            if i & (1<<j): # i의 j번 비트가 1인 경우
+                a.append(arr[j]) # j번 원소 추가
+        if len(a)==N and sum(a)==K: # 완성된 부분집합이 조건을 만족하면
+            re+=1 # 개수 +1
+    
+    print(f'#{t} {re}') # 최종 결과 출력
