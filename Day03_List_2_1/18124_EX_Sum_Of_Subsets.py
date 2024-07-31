ae170689af063959ea5512521736e16498444683
PoloCeleste <voidness12@gmail.com>
2024-07-31 17:49:12 +0900

    update

---
+++ Day03_List_2_1/18124_EX_Sum_Of_Subsets.py
@@ -0,0 +1,15 @@
+﻿n = 10 # 원소의 개수
+
+for t in range(1, int(input())+1): # 테스트 케이스만큼 루프
+    arr = list(map(int, input().split()))
+    re=0 # 해당하는 부분집합 개수
+    for i in range(1, 1<<n): # 1<<n : 부분 집합의 개수 (2진수)
+        a=[]
+        for j in range(n): # 원소의 수만큼 비트를 비교함
+            if i & (1<<j): # i의 j번 비트가 1인 경우
+                a.append(arr[j]) # j번 원소 추가
+        if sum(a)==0: # 완성된 부분집합이 조건을 만족하면
+            re+=1 # 개수 +1
+    
+    if re != 0: re=1
+    print(f'#{t} {re}') # 최종 결과 출력
--- Day03_List_2_1/1979_Word_Puzzle.py
+++ Day03_List_2_1/1979_Word_Puzzle.py
@@ -1,8 +1,12 @@
-﻿for t in range(1, int(input())+1):
-    N , K = map(int, input().split())
+﻿import sys
+sys.stdin = open('input.txt', 'r')
+
+
+for t in range(1, int(input())+1):
+    N , K = map(int, sys.stdin.readline().split())
     
-    arr = [list(map(int, input().split())) for _ in range(N)]
-    
+    arr = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+    re = 0
     for i in range(N):
         for j in range(N-K+1):
             uj = 0
@@ -10,10 +14,32 @@
                 uj+=arr[i][j+k]
             if uj == k:
                 try:
-                    t=0
-                    if arr[i][j-1] == 0:
-                        pass
-                except:
-                    pass
+                    if arr[i][j-1]==0 and arr[i][j+k+1]==0:
+                        re+=1
+                except IndexError:
+                    if 0<=(j-1):# and (j+k+1)==N:
+                        if arr[i][j-1]==0:
+                            re+=1
+                    elif (j+k+1)<N:# and 0>(j-1):
+                        if arr[i][j+k+1]==0:
+                            re+=1
+    for i in range(N-K+1):
+        for j in range(N):
+            uj = 0
+            for k in range(K):
+                uj+=arr[i+k][j]
+            if uj == k:
+                try:
+                    if arr[i-1][j]==0 and arr[i+k+1][j]==0:
+                        re+=1
+                except IndexError:
+                    if 0<=(i-1):# and (i+k+1)==N:
+                        if arr[i-1][j]==0:
+                            re+=1
+                    elif (i+k+1)<N:# and 0>(i-1):
+                        if arr[i+k+1][j]==0:
+                            re+=1
+    
+    print(f'#{t} {re}')
             
                 
