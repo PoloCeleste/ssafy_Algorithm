5e0329f23b454e32b03fe49315e109f8d9d79b1f
PoloCeleste <voidness12@gmail.com>
2024-08-04 14:46:16 +0900

    update Gravity

--- Day02_List_1_2/16504_Gravity.py
+++ Day02_List_1_2/16504_Gravity.py
@@ -1,42 +1,38 @@
-﻿# import copy
-# def bubble_sort(origin, dump):
-#     cnt=[0]*dump
-#     for i in range(dump-1, 0, -1):
-#         arr=copy.deepcopy(origin)
-#         for j in range(i):
-#             if arr[j] > arr[j+1]:
-#                 arr[j], arr[j+1] = arr[j+1], arr[j]
-#                 cnt[i]+=1
-#     return cnt  ##### 고정된 상태에서 비교해야 가능
-#### 버블정렬은 계속 바뀌므로 불가
+﻿def bubble_sort(origin, dump):
+    arrn = origin[:]
+    for i in range(dump-1, 0, -1):
+        for j in range(i):
+            if arrn[j] > arrn[j + 1]:
+                arrn[j], arrn[j + 1] = arrn[j + 1], arrn[j]
+    return arrn
 
-# T = int(input())
-# for i in range(1, T+1):
-#     dump = int(input())
-#     arr=list(map(int, input().split()))
-#     if dump==1:
-#         print(f'#{i} 0')
-#         continue
-#     cnt=bubble_sort(arr, dump)
-#     mx =0
-#     for j in range(dump):
-#         print(dump-(j+1)-cnt[j])
-#         if mx<=cnt[j]:mx=cnt[j]
-#     print(f'#{i} {mx}')
-    
-    
-# for t in range(1, int(input())+1): #테스트케이스
-#     N = int(input()) # 가로 길이
-#     boxes = list(map(int, input().split())) # 건물 배열
 
-#     max_distance = 0 # 최대 낙차
- 
-#     for i in range(N): #길이만큼 반복
-#         cnt = 0 # 낙차 카운트
-#         for j in range(i+1, N): 
-#             if boxes[i] <= boxes[j]: cnt += 1 
-#         print(N - 1 - i - cnt)
-#         max_distance = max(max_distance, N - 1 - i - cnt) # 현재 최대값과  비교해서 최대값 저장
-#         print(max_distance)
- 
-#     print(f'#{t}', max_distance)
+for t in range(1, int(input()) + 1):
+    dump = int(input())
+    arr = list(map(int, input().split()))
+    if dump == 1:
+        print(f'#{t} 0')
+        continue
+    sorted_arr = bubble_sort(arr, dump)
+    mx = 0
+    for x in arr:
+        drop = sorted_arr.index(x) - arr.index(x)
+        if mx < drop: mx = drop
+    print(arr)
+    print(sorted_arr)
+    print(f'#{t} {mx}')
+
+# 고정된 상태에서 비교해야 가능
+# 버블 정렬은 배열이 계속 바뀌므로 불가
+# 새로운 배열 생성하고 기존 배열과 인덱스 비교
+
+# for t in range(1, int(input())+1):
+#     N = int(input())
+#     arr = list(map(int,input().split()))
+#     mx = 0
+#     for i in range(N):
+#         cnt = 0
+#         for j in range(i+1, N):
+#             if arr[i] > arr[j]: cnt += 1
+#         if mx < cnt: mx = cnt
+#     print(f'#{t} {mx}')
