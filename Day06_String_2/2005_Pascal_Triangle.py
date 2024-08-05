c4652cad95f5ceb2aef4ee0953b34b4f1213729a
PoloCeleste <voidness12@gmail.com>
2024-08-05 12:25:45 +0900

    upload Pascal's Triangle

---
+++ Day06_String_2/2005_Pascal_Triangle.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('pascal_input.txt', 'r')
+
+for t in range(1, int(input())+1):
+    N = int(input())
+    print(f'#{t}')
+    arr=[[1]]
+    for i in range(1, N):
+        tmp = [0]*(i+1)
+        for j in range(i+1):
+            c = 0
+            if j-1>=0: c += arr[i-1][j-1]
+            try: c += arr[i-1][j]
+            except IndexError: pass
+            tmp[j]=c
+        arr.append(tmp)
+
+    for a in arr:
+        print(*a)
