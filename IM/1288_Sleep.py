241c9b9c430e68bae03c37acfff9afa7d8f07c3d
PoloCeleste <voidness12@gmail.com>
2024-08-14 11:36:16 +0900

    update 1288 08.14

--- IM/1288_Sleep.py
+++ IM/1288_Sleep.py
@@ -1,16 +1,17 @@
+import sys
+sys.stdin = open('1288_input.txt', 'r')
+
 for t in range(int(input())):
-    N=input()
-    if N=='1':
-        print(10)
+    N = input()
+    if N == '1':
+        print(f'#{t+1} 10')
         continue
-    l=set()
-    x=1
-    result=0
+    l, x, result = set(), 1, 0
     while 1:
+        result += 1
         for n in range(int(N), int(N)*x+1, int(N)):
-            result += 1
             for y in str(n):
                 l.add(int(y))
-        if len(l)==10:break
-        x+=1
-    print(result)
+        if len(l) == 10: break
+        x += 1
+    print(f'#{t+1} {result*int(N)}')
