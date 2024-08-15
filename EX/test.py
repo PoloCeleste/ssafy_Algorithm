fb0c34e1ca20b7138ea594f45f1a69e1b1c69e9b
PoloCeleste <voidness12@gmail.com>
2024-08-15 14:24:17 +0900

    update test.py 08.14

--- EX/test.py
+++ EX/test.py
@@ -1,19 +1,27 @@
 ﻿from math import factorial
-import sys
-from time import time
+import sys, cProfile
+# from time import time
 
 sys.stdin = open("sample_input (1).txt", "r")
 def dfs(depth, l_sum, r_sum, result):
         r = result
+        # st0=time()
         if l_sum < r_sum: return r
+        # print('if/',l_sum,r_sum,time()-st0,depth)
         if l_sum >= total_w / 2:
             r += (1 << (N - depth)) * factorial(N - depth)
             return r
         for i in range(N):
             if visited[i]: continue
             visited[i] = 1
+            # st1=time()
+            # print('1-0/',depth,st1-st0)
             r = dfs(depth + 1, l_sum + arr[i], r_sum, r)
+            # print('w1/',r,time()-st1,depth)
+            # st2=time()
+            # print('2-1/',depth,st2-st1)
             r = dfs(depth + 1, l_sum, r_sum + arr[i], r)
+            # print('w2/',r,time()-st2,depth)
             visited[i] = 0
         return r
 if __name__ == '__main__': 
@@ -23,10 +31,11 @@ if __name__ == '__main__':
         result = 0
         visited = [0] * N
         total_w = sum(arr)
-        st = time()
+        # st = time()
+        # result = dfs(0, 0, 0, result)
         for n in range(N):
             visited[n] = 1
             result = dfs(1, arr[n], 0, result)
             visited[n] = 0
-        print(time()-st)
+        # print('all/',time()-st)
         print(f"#{test_case} {result}")
