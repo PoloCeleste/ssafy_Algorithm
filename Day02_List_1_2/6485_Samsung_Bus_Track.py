f6db9cb4e684c3b29c262f0ef76dcc33a767f205
PoloCeleste <voidness12@gmail.com>
2024-08-16 09:31:00 +0900

    update 6485

--- Day02_List_1_2/6485_Samsung_Bus_Track.py
+++ Day02_List_1_2/6485_Samsung_Bus_Track.py
@@ -6,20 +6,16 @@
         arr.append(s)
     P = int(input())
     c=[0]*P
-    for j in range(P):
-        c[j]=int(input())
+    for j in range(P): c[j]=int(input())
     result={}
     result_l=[]
     for x in range(N):
         for y in range(arr[x][0], arr[x][1]+1):
-            if y in result:
-                result[y]+=1
-            else : result[y]=1
+            if y in result: result[y]+=1
+            else: result[y]=1
     for a in c:
-        if a in result:
-            result_l.append(result[a])
-        else:
-            result_l.append(0)
+        if a in result: result_l.append(result[a])
+        else: result_l.append(0)
 
     print(f"#{i}", end=' ')
     print(*result_l)
