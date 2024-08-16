5c87c5356335b212a1996970da8377e309fd5fd3
PoloCeleste <voidness12@gmail.com>
2024-08-16 09:31:21 +0900

    modifying 10761

--- IM/10761_Trust.py
+++ IM/10761_Trust.py
@@ -19,44 +19,32 @@ for t in range(1, int(input())+1):
         elif arr[i]=='O':
             O_road[int(arr[i+1])]=(i//2)
             btno.append(int(arr[i+1]))
-    bdic, odic={}, {}
-    for b in btnb:
-        BO=[o for o in btno if b>=o]
-        bdic[b]=BO
-    for o in btno:
-        OB=[b for b in btnb if o>=b]
-        odic[o]=OB
-    btn['b'], btn['o'] = bdic, odic
-    def Robot(C_road, C, check, val, cnt, info=True):
+    btn['b'], btn['o'] = btnb, btno
+    def Robot(C_road, C, check, cnt, info=True):
         global btn
         if C_road[C]==cnt:
-            x=[w for w in btn[check][C]]
-            if x:
-                if C not in x:return C, True, cnt, C_road
             if not info: return C, True, cnt, C_road
-            for value in btn[val].values():
-                if C in value:value.remove(C)
             cnt+=1
             C_road[C]=-1
             if check=='b':
-                if cnt==O_road[O]:return C, False, cnt, C_road
+                if cnt==O_road[O]:return C, False, cnt, C_road #
                 else: return C, True, cnt, C_road
-            else:
-                if cnt==B_road[B]:return C, False, cnt, C_road
-                else: return C, True, cnt, C_road
+            else: return C, True, cnt, C_road
         else:
+            # 현 위치가 눌러야할 버튼이 아닐 경우 만약 해당 길의 다음버튼 위치이면 멈춰서 기다리고
+            # 아무것도 없다면 다음 위치를 향해 이동
+            # 다음 위치가 없다면 계속 위로 이동
             for d in [s for s in range(cnt, N)]:
                 if C_road[C]==d:
                     return C, True, cnt, C_road
                 elif d in C_road:
-                    if C_road.index(d)>C:
-                        return C+1, True, cnt, C_road
+                    if C_road.index(d)>C: return C+1, True, cnt, C_road
                     else: return C-1, True, cnt, C_road
             else: return C+1, True, cnt, C_road
 
     while cnt!=N:
         time+=1
-        B, Orange, cnt, B_road=Robot(B_road, B, 'b','o', cnt)
-        O, _, cnt, O_road=Robot(O_road, O, 'o','b', cnt, Orange)
+        B, Orange, cnt, B_road=Robot(B_road, B, 'b', cnt)
+        O, _, cnt, O_road=Robot(O_road, O, 'o', cnt, Orange)
 
     print(f'#{t} {time}')
