85bae3775213fa8a686fe7609bd4a173941fb020
PoloCeleste <voidness12@gmail.com>
2024-08-25 22:58:50 +0900

    update 1226 08.25

--- Day12_Queue_2/1226_maze1.py
+++ Day12_Queue_2/1226_maze1.py
@@ -1,6 +1,6 @@
 ﻿import sys
-from collections import deque
 sys.stdin=open('1226_input.txt','r')
+from collections import deque
 N=16
 dij=[[0,1],[1,0],[0,-1],[-1,0]]
 
@@ -10,21 +10,28 @@ def find():
             if arr[i][j]==2:return [i,j]
 
 def maze(start, visited, path):
+    global result
+    if result:return
     visited[start[0]][start[1]]=1
     for di, dj in dij:
         ni, nj=di+start[0], dj+start[1]
         if ni<0 or ni>=N or nj<0 or nj>=N:continue
-        if arr[ni][nj]==3:return 1
+        if arr[ni][nj]==3:
+            result=1
+            return
         if arr[ni][nj]==1 or visited[ni][nj]:continue
         path.append([ni,nj])
-    if not path:return 0
+    if not path:return
     start=path.popleft()
     maze(start, visited, path)
+
 
 for T in range(10):
     t=int(input())
     arr=[list(map(int, input())) for _ in range(N)]
     start=find()
-    visited=[[-1]*N for _ in range(N)]
+    visited=[[0]*N for _ in range(N)]
     path=deque()
-    print(f'#{t} {maze(start, visited, path)}')
+    result=0
+    maze(start, visited, path)
+    print(f'#{t} {result}')
