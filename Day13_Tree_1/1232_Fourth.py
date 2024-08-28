db4f176958f99c7be7b06b6b1c88784d92790a34
PoloCeleste <voidness12@gmail.com>
2024-08-28 14:46:35 +0900

    modified 1232 08.28

--- Day13_Tree_1/1232_Fourth.py
+++ Day13_Tree_1/1232_Fourth.py
@@ -13,6 +13,41 @@ def is_num(c):
     else:return True
 
 def postorder_traverse(T):
+    l=r=0.0
+    if T:
+        if T.left:
+            postorder_traverse(arr[T.left])
+            l=arr[T.left].char
+        if T.right:
+            postorder_traverse(arr[T.right])
+            r=arr[T.right].char
+        if is_num(l) and is_num(r):
+            if T.char=='+':T.char=float(l)+float(r)
+            elif T.char=='-':T.char=float(l)-float(r)
+            elif T.char=='*':T.char=float(l)*float(r)
+            elif T.char=='/':T.char=float(l)/float(r)
+
+for t in range(1, 11):
+    N=int(input())
+    arr=[0]*(N+1)
+    for n in range(1, N+1):
+        node, *inp = input().split()
+        if len(inp)==3:
+            char, left, right=inp
+            arr[int(node)] = tree(char, left, right)
+        elif len(inp)==2:
+            char, left=inp
+            arr[int(node)] = tree(char, left)
+        else:
+            char=inp[0]
+            arr[int(node)]=tree(char)
+
+    postorder_traverse(arr[1])
+    print(f'#{t} {int(arr[1].char)}')
+
+
+"""
+def postorder_traverse(T):
     if T:
         if T.left:postorder_traverse(arr[T.left])
         if T.right:postorder_traverse(arr[T.right])
@@ -46,5 +81,5 @@ for t in range(1, 11):
             elif r=='-':cal.append(a-b)
             elif r=='*':cal.append(a*b)
             elif r=='/':cal.append(a/b)
-
     print(f'#{t} {int(cal.pop())}')
+"""
