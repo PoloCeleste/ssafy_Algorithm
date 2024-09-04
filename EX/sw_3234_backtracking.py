bf5e9ddb1a1f95216e00d0fc6c5d2b50d4a85c2d
PoloCeleste <voidness12@gmail.com>
2024-09-04 17:14:14 +0900

    upload 3234 09.04 17:14

--- EX/sw_3234_backtracking.py
+++ EX/sw_3234_backtracking.py
@@ -50,3 +50,66 @@ for test_case in range(1, T + 1):
 
 et = time()
 print(et - st)
+
+
+# for 문 사용
+from math import factorial
+
+def dfs(depth, l_sum, r_sum, result):
+    r = result
+    if l_sum < r_sum: return r
+    if l_sum >= total_w / 2:
+        r += (1 << (N - depth)) * factorial(N - depth)
+        return r
+    for i in range(N):
+        if visited[i]: continue
+        visited[i] = 1
+        r = dfs(depth + 1, l_sum + arr[i], r_sum, r)
+        r = dfs(depth + 1, l_sum, r_sum + arr[i], r)
+        visited[i] = 0
+    return r
+
+
+if __name__ == '__main__':
+    for test_case in range(1, int(input()) + 1):
+        N = int(input())
+        arr = list(map(int, input().split()))
+        result = 0
+        visited = [0] * N
+        total_w = sum(arr)
+        for n in range(N):
+            visited[n] = 1
+            result = dfs(1, arr[n], 0, result)
+            visited[n] = 0
+        print(f"#{test_case} {result}")
+
+
+
+# 바로 호출
+from math import factorial
+
+
+def dfs(depth, l_sum, r_sum, result):
+    r = result
+    if l_sum < r_sum: return r
+    if l_sum >= total_w / 2:
+        r += (1 << (N - depth)) * factorial(N - depth)
+        return r
+    for i in range(N):
+        if visited[i]: continue
+        visited[i] = 1
+        r = dfs(depth + 1, l_sum + arr[i], r_sum, r)
+        r = dfs(depth + 1, l_sum, r_sum + arr[i], r)
+        visited[i] = 0
+    return r
+
+
+if __name__ == '__main__':
+    for test_case in range(1, int(input()) + 1):
+        N = int(input())
+        arr = list(map(int, input().split()))
+        result = 0
+        visited = [0] * N
+        total_w = sum(arr)
+        result = dfs(0, 0, 0, result)
+        print(f"#{test_case} {result}")
