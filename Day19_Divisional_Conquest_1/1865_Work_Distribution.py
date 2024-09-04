80aa5f6738e1e2fac180b219d03263fb145b8d03
PoloCeleste <voidness12@gmail.com>
2024-09-04 16:23:05 +0900

    upload 1865 WorkDistribution 09.04 16:23

--- Day19_Divisional_Conquest_1/1865_Work_Distribution.py
+++ Day19_Divisional_Conquest_1/1865_Work_Distribution.py
@@ -1,6 +1,7 @@
 import sys
 sys.stdin=open('1865_input.txt', 'r')
 
+# Ver.1  24s
 for t in range(1, int(input())+1):
     N=int(input())
     co=[c for c in range(N)]
@@ -33,5 +34,34 @@ for t in range(1, int(input())+1):
             visited[i]=False
 
     f([])
+
+    print(f'#{t} {result:.06f}')
+
+# ver.2  6s
+for t in range(1, int(input())+1):
+    N=int(input())
+    arr=[list(map(int, input().split())) for _ in range(N)]
+    if N==1:
+        print(f'#{t} {arr[0][0]:.06f}')
+        continue
+    result=0
+    visited=[False]*N
+
+    def f(n,cur_mul):
+        if cur_mul==0:return
+        global result
+        t=cur_mul*100
+        if n==N:
+            result=max(t,result)
+            return
+        if t<result:return
+
+        for i in range(N):
+            if visited[i]:continue
+            visited[i]=True
+            f(n+1, cur_mul*arr[n][i]/100)
+            visited[i]=False
+
+    f(0,1)
 
     print(f'#{t} {result:.06f}')
